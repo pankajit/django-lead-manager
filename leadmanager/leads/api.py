3eaac4dcde9ffd5754a1f97c984137bb8d581135
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-28 02:09:26 -0400

    Creates an 'api.py' folder inside of the 'leads' directory

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,12 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset => allows us to create a full CRUD API without having to
+#   specify explicit methods for the functionality
+class LeadViewSet(viewsets.ModelViewSet):
+  queryset = Lead.objects.all() # Gets all the leads
+  permission_classes = [
+    permissions.AllowAny # allows anyone to access your leads
+  ]
+  serializer_class = LeadSerializer # the serializer that we created
