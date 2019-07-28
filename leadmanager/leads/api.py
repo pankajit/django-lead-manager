f90bea11a23e70de95cf4159d6de76a1b64f7149
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-28 02:21:10 -0400

    Defines 'urlpatterns' in urls.py

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -5,8 +5,8 @@ from .serializers import LeadSerializer
 # Lead Viewset => allows us to create a full CRUD API without having to
 #   specify explicit methods for the functionality
 class LeadViewSet(viewsets.ModelViewSet):
-  queryset = Lead.objects.all() # Gets all the leads
-  permission_classes = [
-    permissions.AllowAny # allows anyone to access your leads
-  ]
-  serializer_class = LeadSerializer # the serializer that we created
+    queryset = Lead.objects.all() # Gets all the leads
+    permission_classes = [
+        permissions.AllowAny # allows anyone to access your leads
+    ]
+    serializer_class = LeadSerializer # the serializer that we created
